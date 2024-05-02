17012939a5bae2cc2e0f9087fce5a461185dabe1
esraashabana <104281057+esraashabana@users.noreply.github.com>
2024-05-02 17:25:39 +0300

    add secure login

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Day2.Controller
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AccountController : ControllerBase
+	{
+		[HttpPost]
+		public IActionResult Login([FromBody]LoginModel loginModel)
+		{
+			if (loginModel.Email == "esraa@gmail.com" && loginModel.Password == "123456")
+			{
+				List<Claim> userData = new List<Claim>();
+				userData.Add(new Claim(ClaimValueTypes.Email, loginModel.Email));
+				userData.Add(new Claim("email", "esraa@gmail.com"));
+				userData.Add(new Claim("password", "123456"));
+				userData.Add(new Claim(ClaimTypes.Role, "admin"));
+
+				var secretKey = "This is my secret keyyyyyyyyyyyyyyy";
+				var Key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey));
+				var HashingSecretKey = new SigningCredentials(Key, SecurityAlgorithms.HmacSha256);
+
+				var token = new JwtSecurityToken(
+					claims: userData,
+					expires: DateTime.Now.AddDays(1),
+					signingCredentials: HashingSecretKey
+					);
+
+				var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+				return Ok(tokenString);
+			}
+			return Unauthorized();
+		}
+		public class LoginModel
+		{
+			public string Email { get; set; }
+			public string Password { get; set; }
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using NewsProject.Models;
 using NewsProject.Repositories;
 
@@ -12,7 +13,6 @@ namespace News
 			var builder = WebApplication.CreateBuilder(args);
 
 			// Add services to the container.
-
 			builder.Services.AddControllers();
 			builder.Services.AddEndpointsApiExplorer();
 			builder.Services.AddSwaggerGen();
@@ -20,6 +20,33 @@ namespace News
 			{
 				options.UseLazyLoadingProxies().UseSqlServer("Server=.;Database = NewsDB; Integrated Security = True; TrustServerCertificate = True; ");
 			});
+			builder.Services.AddCors(options =>
+			{
+				options.AddPolicy("CorsAllowed",
+				builder =>
+				{
+					builder.AllowAnyOrigin();
+					builder.AllowAnyMethod();
+					builder.AllowAnyHeader();
+				});
+			});
+			builder.Services.AddAuthentication(option => option.DefaultAuthenticateScheme = "myscheme")
+			   .AddJwtBearer("myscheme",
+			   op =>
+			   {
+				   #region secret key
+				   string key = "This is my secret keyyyyyyyyyyyyyyy";
+				   var secertkey = new SymmetricSecurityKey(System.Text.Encoding.ASCII.GetBytes(key));
+				   #endregion
+				   op.TokenValidationParameters = new TokenValidationParameters()
+				   {
+					   IssuerSigningKey = secertkey,
+					   ValidateIssuer = false,
+					   ValidateAudience = false
+
+				   };
+			   });
+
 			builder.Services.AddScoped<IAuthors, AuthorsRepo>();
 			builder.Services.AddScoped<INewsData, NewsRepo>();
 			var app = builder.Build();
@@ -30,12 +57,11 @@ namespace News
 				app.UseSwagger();
 				app.UseSwaggerUI();
 			}
-
+			app.UseCors("CorsAllowed");
+			app.UseHttpsRedirection();
+			app.UseAuthentication();
 			app.UseAuthorization();
-
-
 			app.MapControllers();
-
 			app.Run();
 		}
 	}
